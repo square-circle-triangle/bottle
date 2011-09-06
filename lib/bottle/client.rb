3f6a28bb567fd1d77e428c7c238d2ea0f9ba7345
Nick Marfleet <nick@sct.com.au>
2011-09-07 08:41:21 +1000

    idea

--- lib/bottle/client.rb
+++ lib/bottle/client.rb
@@ -29,5 +29,35 @@ module Bottle
         end
       end
     end
+    
+    ##### XXXXXXXXXXXX ###########
+    
+    def with_connection
+      with_amqp do
+        @publisher = Bottle::Publisher.new(@channel, @channel.default_exchange, @reply_queue_name)   
+        @close_connections = false
+        yield self
+        @close_connections = true
+        close_connection
+      end
+    end
+    
+    def dispatch(msg_type, payload = {}, opts={})
+        args = [payload.to_yaml, :routing_key => @queue_name, :type => msg_type]
+
+        if block_given?
+          @publisher.publish(*args) do |data|
+            log.debug "Passing on the response..."
+            yield(data)
+            close_connection if @close_connections
+          end 
+        else
+          @publisher.publish(*args)
+          # How to know when it is safe to close?  Need a callback to know the msg has gone?
+          # close_connection if @close_connections
+        end
+      end
+    end
+    
   end
 end
