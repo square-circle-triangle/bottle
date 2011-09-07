28a858bb42423020e6854a75b3e064e100dcd826
Nick Marfleet <nick@sct.com.au>
2011-09-08 09:34:23 +1000

    starting publisher specs

--- specs/bottle/publisher_spec.rb
+++ specs/bottle/publisher_spec.rb
@@ -1,8 +1,31 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe Bottle::Publisher do
-  describe 'new' do
-    it "should setup ivars"
+  include EventedSpec::SpecHelper
+
+  em_before { AMQP.cleanup_state }
+  em_after  { AMQP.cleanup_state }
+
+  before :each do
+    @amqptest = AmqpTest.new
+    @exchange = @amqptest.channel.default_exchange
+    @reply_queue_name = "99.bottles"
+  end
+
+  describe "new" do
+
+    it "should setup ivars" do
+      em do
+        @amqptest.with_amqp do
+          args = {}
+          publisher = described_class.new(@amqptest.channel, @exchange, @reply_queue_name)
+          publisher.reply_queue_name.should == @queue_name
+          publisher.channel.should == @amqptest.channel
+          publisher.exchange.should  == @exchange
+          default_done
+        end
+      end
+    end
   end
   
   describe "publish" do
